d961e38217c2b95953d98bec0fc5d2419d1010bc
Deign <robin.reigstad@gmail.com>
2018-02-23 06:19:30 -0800

    Create a todo list

--- SwarmOS/src/Consts/SwarmConsts.ts
+++ SwarmOS/src/Consts/SwarmConsts.ts
@@ -1,10 +1,2 @@
 export const MY_USERNAME = 'Deign';
-export const MY_SIGNATURE = 'Do not stand in the way of The Swarm\nSwarmOS v0.5';
-
-export const MINI_HARVESTER = [WORK, WORK, MOVE, CARRY];
-export const PRIME_HARVESTER = [WORK, WORK, WORK, WORK, WORK, MOVE];
-export const BUILDER = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE];
-export const REPAIRER = [WORK, CARRY, CARRY, CARRY, MOVE, MOVE];
-export const MINI_DELIVERER = [MOVE, CARRY];
-export const BIG_DELIVERY = [CARRY, CARRY, MOVE, MOVE, CARRY, CARRY, MOVE, MOVE, CARRY, CARRY, MOVE, MOVE, CARRY, CARRY, MOVE, MOVE]
-export const UPGRADER = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+export const MY_SIGNATURE = 'Do not stand in the way of The Swarm\nSwarmOS v0.7';
