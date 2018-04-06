2bd60ca3f3fa007d97cf8f484dbc743574188234
Deign <robin.reigstad@gmail.com>
2018-04-06 04:23:50 -0700

    control part1

---
+++ SwarmOS/src/Consuls/ControlConsul.ts
@@ -0,0 +1,6 @@
+import { ControlMemory } from "SwarmMemory/ConsulMemory";
+import { SwarmConsul } from "Consuls/ConsulBase";
+
+export class ControlConsul extends SwarmConsul<ControlMemory> {
+
+}
--- SwarmOS/src/SwarmMemory/ConsulMemory.ts
+++ SwarmOS/src/SwarmMemory/ConsulMemory.ts
@@ -1,13 +1,18 @@
 import { MemoryBase } from "./SwarmMemory";
 
-export type TConsulMemory = HarvestMemory;
+export type TConsulMemory = HarvestMemory | ControlMemory;
 
-export abstract class ConsulMemory<T extends ConsulType> extends MemoryBase<TConsulData> {
+export abstract class ConsulMemory<T extends TConsulData> extends MemoryBase<T> {
     get MEM_TYPE(): SwarmDataType.Consul { return SwarmDataType.Consul }
     get SWARM_TYPE(): SwarmType.SwarmConsul { return SwarmType.SwarmConsul }
-    abstract get SUB_TYPE(): T;
+    abstract get SUB_TYPE(): ConsulType;
 }
-export class HarvestMemory extends ConsulMemory<ConsulType.Harvest> implements HarvestConsulData {
+export class HarvestMemory extends ConsulMemory<HarvestConsulData> implements HarvestConsulData {
     get sourceIDs() { return this._cache.sourceIDs; }
     get SUB_TYPE(): ConsulType.Harvest { return ConsulType.Harvest; }
+}
+
+export class ControlMemory extends ConsulMemory<ControlConsulData> implements ControlConsulData {
+    get creepIDs(): string[] { return this._cache.creepIDs; }
+    get SUB_TYPE(): ConsulType.Control { return ConsulType.Control; }
 }
