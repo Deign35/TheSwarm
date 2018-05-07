c06475fbe541a6ac2edda4aceb4131486510dbd7
Deign <robin.reigstad@gmail.com>
2018-05-06 22:54:06 -0700

    Move ServiceProvider

--- SwarmOS/src/main.ts
+++ SwarmOS/src/main.ts
@@ -22,7 +22,7 @@ import { Kernel } from "Core/Kernel";
 import { ExtensionRegistry } from "Core/ExtensionRegistry";
 import { ProcessRegistry } from "Core/ProcessRegistry";
 
-import { bundle as ServiceBundle } from "./ServiceProvider";
+import { bundle as ServiceBundle } from "Core/ServiceProvider";
 import { bundle as TestBundle } from "PosisTest";
 
 export let extensionRegistry = new ExtensionRegistry();
