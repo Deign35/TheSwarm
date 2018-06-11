fb26a96c4d10e062c5122775aaeceba2dec2faa8
Deign <robin.reigstad@gmail.com>
2018-06-10 21:21:08 -0700

    Setup v1.1 and fix compile error

--- SwarmOS/src/ColonyManagement/index.ts
+++ SwarmOS/src/ColonyManagement/index.ts
@@ -1,9 +1,7 @@
-import { OSPackage as Cartographer } from "./SwarmCartographer";
 import { OSPackage as CLI } from "ColonyManagement/SwarmCLI";
 
 export const ColonyManagementPackage: IPackage<{}> = {
     install(processRegistry: IProcessRegistry, extensionRegistry: IExtensionRegistry) {
-        Cartographer.install(processRegistry, extensionRegistry);
         CLI.install(processRegistry, extensionRegistry);
     }
 }
