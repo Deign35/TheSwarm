a70ee7ec2fae3051c0cefa505a97d524306a5e68
Deign <robin.reigstad@gmail.com>
2018-06-19 00:49:41 -0700

    HarvestRoomJob looking close to complete

--- SwarmOS/src/RoomJobs/HarvestRoomJob.ts
+++ SwarmOS/src/RoomJobs/HarvestRoomJob.ts
@@ -1,3 +1,9 @@
+export const OSPackage: IPackage<SpawnRegistry_Memory> = {
+    install(processRegistry: IProcessRegistry, extensionRegistry: IExtensionRegistry) {
+        processRegistry.register(RJ_Harvest, RoomStateHarvestActivity);
+    }
+}
+
 import { RoomStateActivity } from "./RoomStateActivities";
 import { FindNextTo, FindStructureNextTo } from "Tools/TheFinder";
 
@@ -18,16 +24,16 @@ class RoomStateHarvestActivity extends RoomStateActivity<RoomStateHarvest_Memory
                 this.SpawnHarvester(keys[i]);
             }
 
-            if (this.room) {
-                let source = Game.getObjectById(keys[i]) as Source;
-                if (mem.sup) {
-                    let obj = Game.getObjectById(mem.sup);
-                    if (!obj) {
-                        delete mem.sup;
-                    }
+            if (mem.sup) {
+                let obj = Game.getObjectById(mem.sup);
+                if (!obj) {
+                    delete mem.sup;
                 }
+            }
 
+            if (this.room) {
                 if (!mem.sup) {
+                    let source = Game.getObjectById(keys[i]) as Source;
                     let nearbyContainers = FindStructureNextTo(source.pos, STRUCTURE_CONTAINER);
                     if (nearbyContainers && nearbyContainers.length > 0) {
                         mem.sup = nearbyContainers[0].structure.id;
@@ -67,23 +73,46 @@ class RoomStateHarvestActivity extends RoomStateActivity<RoomStateHarvest_Memory
     SpawnHarvester(sourceID: ObjectID) {
         this.memory.lu = Game.time;
         let spawnLevel = 0;
+        let priority: Priority = Priority_Medium;
         if (this.memory.hr != this.memory.rID) {
             spawnLevel = 2;
+            priority = Priority_Low;
         } else {
             let homeRoom = Game.rooms[this.memory.hr];
             if (homeRoom.energyCapacityAvailable >= CreepBodies.Harvester[2].cost) {
                 spawnLevel = 2;
             } else if (homeRoom.energyCapacityAvailable >= CreepBodies.Harvester[1].cost) {
                 spawnLevel = 1;
+            } else {
+                let source = Game.getObjectById(sourceID) as Source;
+                if (source) {
+                    let nearby = FindNextTo(source.pos, LOOK_TERRAIN);
+                    let hasSpawnedSupport = false;
+                    let hasSpawnedHarvester = false;
+                    for (let i = 0; i < nearby.length; i++) {
+                        if (nearby[i].terrain !== Terrain_Wall) {
+                            if (!hasSpawnedHarvester) {
+                                hasSpawnedHarvester = true;
+                                continue;
+                            }
+                            this.log.info(`Spawning a harvester for ${this.roomData.sourceIDs[i]}.`);
+                            this.SpawnSupport(sourceID);
+                            if (hasSpawnedSupport) {
+                                break;
+                            }
+                            hasSpawnedSupport = true;
+                        }
+                    }
+                }
             }
         }
 
         let spawnID = this.spawnRegistry.requestSpawn({
             l: spawnLevel,
             c: CT_Harvester,
-            n: (sourceID + '_Ha').slice(-5),
+            n: this.memory.rID + '_' + (Game.time + '_Ha').slice(-5),
             p: this.pid,
-        }, this.memory.rID, Priority_Low, 3, {
+        }, this.memory.rID, priority, 3, {
                 ct: CT_Harvester,
                 lvl: spawnLevel,
                 p: this.pid,
@@ -91,92 +120,88 @@ class RoomStateHarvestActivity extends RoomStateActivity<RoomStateHarvest_Memory
             });
         let spawnMem: SpawnActivity_Memory = {
             sID: spawnID,
-            HC: "MainSourceHarvestActivity"
+            HC: "HarvestSourceActivity"
         }
 
         this.memory.harvesters[sourceID].pid = this.kernel.startProcess(SPKG_SpawnActivity, spawnMem);
-        if (spawnLevel == 0) {
-            let source = Game.getObjectById(sourceID) as Source;
-            if (source) {
-                let nearby = FindNextTo(source.pos, LOOK_TERRAIN);
-                let hasSpawnedSupport = false;
-                let hasSpawnedHarvester = false;
-                for (let i = 0; i < nearby.length; i++) {
-                    if (nearby[i].terrain !== 'wall') {
-                        if (!hasSpawnedHarvester) {
-                            hasSpawnedHarvester = true;
-                            continue;
-                        }
-                        this.log.info(`Spawning a harvester for ${this.roomData.sourceIDs[i]}.`);
-                        this.SpawnSupport(sourceID);
-                        if (hasSpawnedSupport) {
-                            break;
-                        }
-                        hasSpawnedSupport = true;
-                    }
-                }
-            }
-        }
     }
 
-    MainSourceHarvestActivity(creepID: CreepID) {
+    HarvestSourceActivity(creepID: CreepID) {
         if (!this.creepRegistry.tryReserveCreep(creepID, this.pid)) {
-            throw new Error('Harvester spawn failed to produce a creep');
-        }
-        let creep = this.creepRegistry.tryGetCreep(creepID, this.pid);
-        if (!creep || !creep.memory.s) {
-            throw new Error('Harvester spawn failed to produce a creep');
-        }
-
-        if (creep.room.name != this.memory.rID) {
-            this.memory.harvesters[creep.memory.s!].pid = this.creepActivity.CreateNewCreepActivity({
-                at: AT_MoveToRoom,
-                p: { x: 25, y: 25, roomName: this.memory.rID },
-                HC: 'SpawnHarvester',
-                CV: creep.memory.s,
-                c: creepID
-            }, this.pid)
             return;
         }
+
+        let creep = this.creepRegistry.tryGetCreep(creepID, this.pid);
+        if (!creep || !creep.memory.s) {
+            this.creepRegistry.releaseCreep(creepID, this.pid);
+            return;
+        }
+
         let source = Game.getObjectById(creep.memory.s) as Source;
+        if (creep.room.name != this.memory.rID) {
+            let map = Game.map.findRoute(creep.room.name, this.memory.rID);
+            let nextRoom = this.memory.rID;
+            if (map == ERR_NO_PATH) {
+                this.log.error(`Could not find a route between two rooms`);
+                return;
+            }
+
+            if (map && map.length > 0) {
+                nextRoom = map[0].room;
+            }
+            let path = creep.pos.findPathTo(new RoomPosition(25, 25, nextRoom), {
+                ignoreCreeps: true,
+                ignoreRoads: true
+            });
+
+            let lastPosition = path[path.length - 1];
+            if (!lastPosition) {
+                this.log.error('Attempted to find a path to the next room, but failed');
+                return;
+            }
+
+            this.memory.harvesters[creep.memory.s].pid = this.creepActivity.CreateNewCreepActivity({
+                at: AT_MoveToPosition,
+                c: creep.name,
+                HC: 'HarvestSourceActivity',
+                p: { x: lastPosition.x, y: lastPosition.y, roomName: creep.room.name }
+            }, this.pid);
+            return;
+        }
+
+        if (!creep.pos.isNearTo(source.pos)) {
+
+        }
+
         let actions: SingleCreepActivity_Memory[] = [];
         let moveTarget = Game.getObjectById<ConstructionSite | StructureContainer>(this.memory.harvesters[creep.memory.s].sup);
         if (!creep.memory.sup && moveTarget) {
-            actions.push({
+            this.memory.harvesters[creep.memory.s].pid = this.creepActivity.CreateNewCreepActivity({
                 at: AT_MoveToPosition,
-                p: moveTarget.pos
-            })
-        } else {
-            actions.push({
-                at: AT_MoveToPosition,
-                p: source.pos,
-                a: 1
-            })
+                p: moveTarget.pos,
+                c: creep.name,
+                HC: 'HarvestSourceActivity'
+            }, this.pid);
+            return;
         }
-
-        actions.push({
-            at: AT_Harvest,
-            t: creep.memory.s,
-            e: [ERR_FULL, ERR_NOT_ENOUGH_RESOURCES]
-        });
 
         let harvMem: RepetitiveCreepActivity_Memory = {
-            a: actions,
+            a: [{
+                at: AT_Harvest,
+                t: creep.memory.s,
+                e: [ERR_FULL, ERR_NOT_ENOUGH_RESOURCES]
+            }],
+            HC: 'HarvestSourceActivity',
             c: creepID
         }
-        if (!creep.memory.sup) {
-            harvMem.CV = creep.memory.s
-            harvMem.HC = 'SpawnHarvester';
-        }
-
-        this.kernel.startProcess(SPKG_RepetitiveCreepActivity, harvMem);
+        this.memory.harvesters[creep.memory.s].pid = this.kernel.startProcess(SPKG_RepetitiveCreepActivity, harvMem);
     }
 
     SpawnSupport(sourceID: ObjectID) {
         let spawnID = this.spawnRegistry.requestSpawn({
             l: 0,
             c: CT_Harvester,
-            n: (sourceID + '_Ha').slice(-5),
+            n: GetSUID() + '_HS',
             p: this.pid
         }, this.memory.rID, Priority_Low, 3, {
                 ct: CT_Harvester,
@@ -187,9 +212,8 @@ class RoomStateHarvestActivity extends RoomStateActivity<RoomStateHarvest_Memory
             });
         let spawnMem: SpawnActivity_Memory = {
             sID: spawnID,
-            HC: "MainSourceHarvestActivity"
+            HC: "HarvestSourceActivity"
         }
-
         this.kernel.startProcess(SPKG_SpawnActivity, spawnMem);
     }
 }
--- SwarmOS/src/RoomJobs/index.ts
+++ SwarmOS/src/RoomJobs/index.ts
@@ -2,9 +2,11 @@ import { OSPackage as RoomProvider } from "RoomJobs/RoomProvider";
 import { OSPackage as TowerJob } from "RoomJobs/TowerJob";
 import { OSPackage as RoomStateActivities } from "RoomJobs/RoomStateActivities";
 import { OSPackage as RoomWorkerTargeting } from "RoomJobs/RoomStateWorkerActivity";
+import { OSPackage as HarvesterRoomJob } from "RoomJobs/HarvestRoomJob";
 
 export const RoomJobs: IPackage<{}> = {
     install(processRegistry: IProcessRegistry, extensionRegistry: IExtensionRegistry) {
+        HarvesterRoomJob.install(processRegistry, extensionRegistry);
         RoomProvider.install(processRegistry, extensionRegistry);
         RoomStateActivities.install(processRegistry, extensionRegistry);
         RoomWorkerTargeting.install(processRegistry, extensionRegistry);
