3ee35ddbbe112611b3b767aaa2731ccdeb8a1e88
Deign <robin.reigstad@gmail.com>
2018-07-04 19:44:55 -0700

    Updated Memory test

--- SwarmOS/test/Memory/SwarmMemory.js
+++ SwarmOS/test/Memory/SwarmMemory.js
@@ -1,57 +1,144 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-var SwarmMemory = /** @class */ (function () {
-    function SwarmMemory(MemoryID, Parent) {
-        this.MemoryID = MemoryID;
-        this.Parent = Parent;
-        this._cache = {};
-        this.Load();
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+class ExtensionBase {
+    constructor() {}
+}
+const SEPERATOR = '/';
+class FileRegistry extends ExtensionBase {
+    get memory() {
+        if (!Memory.FileSystem) {
+            Memory.FileSystem = {};
+        }
+        return Memory.FileSystem;
     }
-    SwarmMemory.prototype.GetData = function (id) { return this._cache[id] || undefined; };
-    SwarmMemory.prototype.SetData = function (id, data) { this._cache[id] = data; };
-    SwarmMemory.prototype.DeleteData = function (id) { delete this._cache[id]; };
-    SwarmMemory.prototype.Save = function () {
-        if (this._cache) {
-            if (this.Parent) {
-                this.Parent.SetData(this.MemoryID, this._cache);
+    get MemCache() {
+        if (!this._memoryCache) {
+            this._memoryCache = {
+                c: {},
+                f: {},
+                p: '',
+            };
+            let keys = Object.keys(this.memory);
+            while (keys.length > 0) {
+                let memPath = keys.shift();
+                if (!memPath || !this.memory[memPath]) {
+                    continue;
+                }
+                let file = this.memory[memPath];
+                let fileName = memPath.slice(memPath.lastIndexOf(SEPERATOR) + 1);
+                let filePath = memPath.substr(0, memPath.length - fileName.length - 1);
+                this.EnsurePath(filePath);
+                this.SaveFile(filePath, fileName, file);
             }
-            else {
-                Memory[this.MemoryID] = this._cache;
-            }
         }
-    };
-    SwarmMemory.prototype.Load = function () {
-        if (this.Parent) {
-            this._cache = this.Parent.GetData(this.MemoryID) || {};
-        }
-        else {
-            this._cache = Memory[this.MemoryID] || {};
-        }
-    };
-    return SwarmMemory;
-}());
-exports.SwarmMemory = SwarmMemory;
-var SwarmLedger = /** @class */ (function (_super) {
-    __extends(SwarmLedger, _super);
-    function SwarmLedger() {
-        return _super !== null && _super.apply(this, arguments) || this;
+        return this._memoryCache;
     }
-    SwarmLedger.prototype.ForEach = function (inFunc) {
-        for (var name_1 in this._cache) {
-            inFunc(this._cache[name_1]);
+    get PathCache() {
+        if (!this._pathCache) {
+            this._pathCache = {};
         }
-    };
-    return SwarmLedger;
-}(SwarmMemory));
-exports.SwarmLedger = SwarmLedger;
-//# sourceMappingURL=SwarmMemory.js.map
+        return this._pathCache;
+    }
+    GetFolder(pathStr) {
+        let mem = this.MemCache;
+        let path = this.PathCache[pathStr];
+        if (!path) {
+            path = pathStr.split(SEPERATOR);
+            this.PathCache[pathStr] = path;
+        }
+        for (let i = 1, length = path.length; i < length; i++) {
+            if (mem.c && mem.c[path[i]]) {
+                mem = mem.c[path[i]];
+            } else {
+                return undefined;
+            }
+        }
+        return mem;
+    }
+    EnsurePath(pathStr) {
+        let path = this.PathCache[pathStr];
+        if (!path) {
+            //path = pathStr.split(/[\/\\]/gi);
+            path = pathStr.split(SEPERATOR);
+            this.PathCache[pathStr] = path;
+        }
+        let curPath = '';
+        for (let i = 1; i < path.length; i++) {
+            let nextFolder = path[i];
+            let nextPath = curPath + SEPERATOR + nextFolder;
+            if (!this.GetFolder(nextPath) && !this.CreateFolder(curPath, nextFolder)) {
+                throw new Error(`Failed to create path(${pathStr}).  Loop[${i}](curPath(${curPath}), nextFolder(${nextFolder})`);
+            }
+            curPath = nextPath;
+        }
+    }
+    CreateFolder(path, folderName) {
+        let folder = this.GetFolder(path);
+        if (folder) {
+            if (!folder.c[folderName]) {
+                folder.c[folderName] = {
+                    c: {},
+                    f: {},
+                    p: path,
+                };
+            }
+            return true;
+        }
+        return false;
+    }
+    DeleteFolder(path, folderName) {
+        let folder = this.GetFolder(path);
+        if (folder && folder.c[folderName]) {
+            const fullPath = path + SEPERATOR + folderName;
+            let fileNames = Object.keys(folder.c[folderName].f);
+            for (let i = 0; i < fileNames.length; i++) {
+                this.DeleteFile(fullPath, fileNames[i]);
+            }
+            let childFolders = Object.keys(folder.c[folderName].c);
+            for (let i = 0; i < childFolders.length; i++) {
+                this.DeleteFolder(fullPath, childFolders[i]);
+            }
+            delete folder.c[folderName];
+        }
+    }
+    SaveFile(path, fileName, mem) {
+        let folder = this.GetFolder(path);
+        if (folder) {
+            let filePath = path + SEPERATOR + fileName;
+            this.memory[filePath] = mem;
+            folder.f[fileName] = this.memory[filePath];
+            return true;
+        }
+        return false;
+    }
+    GetFile(path, fileName) {
+        let folder = this.GetFolder(path);
+        if (folder) {
+            return folder.f[fileName] && folder.f[fileName];
+        }
+        return undefined;
+    }
+    DeleteFile(path, fileName) {
+        let folder = this.GetFolder(path);
+        if (folder && folder.f[fileName]) {
+            let fullPath = path + SEPERATOR + fileName;
+            if (this.memory[fullPath]) {
+                delete this.memory[fullPath];
+            }
+            delete folder.f[fileName];
+        }
+    }
+    CopyFile(fromPath, fileName, toPath, deleteOriginal = true, newFileName) {
+        let file = this.GetFile(fromPath, fileName);
+        if (!file) {
+            return false;
+        }
+        if (deleteOriginal) {
+            this.DeleteFile(fromPath, fileName);
+        }
+        return this.SaveFile(toPath, newFileName || fileName, file);
+    }
+}
+exports.FileRegistry = FileRegistry;
--- SwarmOS/test/MiscTestFile.js
+++ SwarmOS/test/MiscTestFile.js
@@ -40,7 +40,38 @@ console.log(iterator.next()); // {value: undefined, done: true},
 
 console.log(result); // "foo"*/
 
+let something = require('./Memory/SwarmMemory');
+global['Memory'] = {};
+let fileSystem = new something.FileRegistry();
+fileSystem.EnsurePath('/TestFolder/TestFolder2');
+fileSystem.SaveFile('/TestFolder', 'TestFile', {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3
+    }
+});
+//fileSystem.CreateFolder('/TestFolder', 'TestFolder2');
+fileSystem.SaveFile('/TestFolder/TestFolder2', 'SubFileTest', {
+    info: 'nothing really'
+});
+console.log(JSON.stringify(Memory));
+fileSystem.CreateFolder('', 'NotACreatedFolder');
+fileSystem.SaveFile('/NotACreatedFolder', 'TestFile2', {
+    data: {
+        info: 'lots of info for realsies'
+    }
+});
 
+console.log(JSON.stringify(fileSystem.GetFile('/NotACreatedFolder', 'TestFile2').data.info));
+fileSystem.CopyFile('/TestFolder/TestFolder2', 'SubFileTest', '/NotACreatedFolder', false, 'TestFile3');
+console.log(JSON.stringify(Memory));
+fileSystem.CopyFile('/TestFolder/TestFolder2', 'SubFileTest', '/NotACreatedFolder', false);
+console.log(JSON.stringify(Memory));
+fileSystem.DeleteFolder('', 'TestFolder');
+console.log(JSON.stringify(Memory));
+
+/*
 
 let logCounter = 0;
 const log = function (message) {
@@ -85,4 +116,4 @@ while (iterator.next().value) {
     if (numYields > 7) {
         runTest = false;
     }
-}
+}*/
