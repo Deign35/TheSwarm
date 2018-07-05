d51601a2d40d9ca9569a6f6110f2ea6918037fe1
Deign <robin.reigstad@gmail.com>
2018-07-04 19:52:39 -0700

    Add some more test infrastructre

---
+++ SwarmOS/test/FileRegistryTests.js
@@ -0,0 +1,30 @@
+let something = require('./Memory/SwarmMemory');
+global['Memory'] = {};
+let fileSystem = new something.FileRegistry();
+fileSystem.EnsurePath('/TestFolder/TestFolder2');
+fileSystem.SaveFile('/TestFolder', 'TestFile', {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3
+    }
+});
+//fileSystem.CreateFolder('/TestFolder', 'TestFolder2');
+fileSystem.SaveFile('/TestFolder/TestFolder2', 'SubFileTest', {
+    info: 'nothing really'
+});
+console.log(JSON.stringify(Memory));
+fileSystem.CreateFolder('', 'NotACreatedFolder');
+fileSystem.SaveFile('/NotACreatedFolder', 'TestFile2', {
+    data: {
+        info: 'lots of info for realsies'
+    }
+});
+
+console.log(JSON.stringify(fileSystem.GetFile('/NotACreatedFolder', 'TestFile2').data.info));
+fileSystem.CopyFile('/TestFolder/TestFolder2', 'SubFileTest', '/NotACreatedFolder', false, 'TestFile3');
+console.log(JSON.stringify(Memory));
+fileSystem.CopyFile('/TestFolder/TestFolder2', 'SubFileTest', '/NotACreatedFolder', false);
+console.log(JSON.stringify(Memory));
+fileSystem.DeleteFolder('', 'TestFolder');
+console.log(JSON.stringify(Memory));
--- SwarmOS/test/MiscTestFile.js
+++ SwarmOS/test/MiscTestFile.js
@@ -38,40 +38,8 @@ console.log(iterator.next()); // {value: 3, done: false}
 console.log(iterator.next()); // {value: undefined, done: true}, 
 // g4() returned {value: 'foo', done: true} at this point
 
-console.log(result); // "foo"*/
+console.log(result); // "foo"
 
-let something = require('./Memory/SwarmMemory');
-global['Memory'] = {};
-let fileSystem = new something.FileRegistry();
-fileSystem.EnsurePath('/TestFolder/TestFolder2');
-fileSystem.SaveFile('/TestFolder', 'TestFile', {
-    data: {
-        a: 1,
-        b: 2,
-        c: 3
-    }
-});
-//fileSystem.CreateFolder('/TestFolder', 'TestFolder2');
-fileSystem.SaveFile('/TestFolder/TestFolder2', 'SubFileTest', {
-    info: 'nothing really'
-});
-console.log(JSON.stringify(Memory));
-fileSystem.CreateFolder('', 'NotACreatedFolder');
-fileSystem.SaveFile('/NotACreatedFolder', 'TestFile2', {
-    data: {
-        info: 'lots of info for realsies'
-    }
-});
-
-console.log(JSON.stringify(fileSystem.GetFile('/NotACreatedFolder', 'TestFile2').data.info));
-fileSystem.CopyFile('/TestFolder/TestFolder2', 'SubFileTest', '/NotACreatedFolder', false, 'TestFile3');
-console.log(JSON.stringify(Memory));
-fileSystem.CopyFile('/TestFolder/TestFolder2', 'SubFileTest', '/NotACreatedFolder', false);
-console.log(JSON.stringify(Memory));
-fileSystem.DeleteFolder('', 'TestFolder');
-console.log(JSON.stringify(Memory));
-
-/*
 
 let logCounter = 0;
 const log = function (message) {
@@ -117,3 +85,4 @@ while (iterator.next().value) {
         runTest = false;
     }
 }*/
+
