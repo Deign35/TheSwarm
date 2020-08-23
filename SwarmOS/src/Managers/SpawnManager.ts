3c7f49c18f3b4e91772e4927fc4813cf929b0a61
Deign <robin.reigstad@gmail.com>
2020-08-23 15:17:11 -0700

    Start SpawnManager

---
+++ SwarmOS/src/Managers/SpawnManager.ts
@@ -0,0 +1,40 @@
+import { BasicProcess, ExtensionBase } from "Core/BasicTypes";
+
+export const OSPackage: IPackage<any> = {
+    install(processRegistry: IProcessRegistry, extensionRegistry: IExtensionRegistry) {
+
+    }
+}
+
+const PKG_SpawnManager_LogContext: LogContext = {
+    logID: PKG_SpawnManager,
+    logLevel: LOG_INFO
+}
+
+// This order determines the default order of body parts
+const BodyLegend = {
+    t: TOUGH,
+    a: ATTACK,
+    r: RANGED_ATTACK,
+    cl: CLAIM,
+    w: WORK,
+    c: CARRY,
+    h: HEAL,
+    m: MOVE,
+}
+
+// (TODO) -- Convert this to auto generated -- maybe also optional ordering??
+// additionally, presize the array.
+const ConvertContextToSpawnBody = function (context: SpawnContext) {
+    let body = [];
+    let bodyDef = CreepBodies.get(context.c)[context.l];
+    for (let bodyID in BodyLegend) {
+        if (bodyDef[bodyID]) {
+            for (let i = 0; i < bodyDef[bodyID]; i++) {
+                body.push(BodyLegend[bodyID]);
+            }
+        }
+    }
+
+    return body;
+}
--- SwarmOS/src/Managers/index.ts
+++ SwarmOS/src/Managers/index.ts
@@ -1,10 +1,15 @@
 import { PackageProviderBase } from "Core/BasicTypes";
 
+import { OSPackage as SpawnManager } from "./SpawnManager";
+
 class SwarmManager extends PackageProviderBase<PackageProviderMemory> {
     protected get RequiredServices(): SDictionary<ProviderService> {
         return this._reqServices;
     }
     private _reqServices: SDictionary<ProviderService> = {
+        spawnManager: {
+            processname: PKG_SpawnManager
+        }
     }
 }
 
