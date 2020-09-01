67d08319af5a1c5d91f08d5d3aabf189f7a9bcab
Deign <robin.reigstad@gmail.com>
2020-08-31 22:39:42 -0700

    Can now withdraw from tombstones

--- SwarmOS/src/Managers/CreepManager.ts
+++ SwarmOS/src/Managers/CreepManager.ts
@@ -283,7 +283,7 @@ class CreepManagerExtensions extends ExtensionBase implements ICreepManagerExten
           }
         }
         return false;
-      case (AT_Withdraw): return ((target as Structure).structureType || (target as Ruin).structure) && !!(target as StructureContainer).store;
+      case (AT_Withdraw): return ((target as Structure).structureType || (target as Ruin).structure || (target as Tombstone).deathTime) && !!(target as StructureContainer).store;
       case (AT_RenewCreep): return ((target as StructureSpawn).structureType && (target as StructureSpawn).structureType == STRUCTURE_SPAWN);
 
       case (AT_Drop):
