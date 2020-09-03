d835edf5e2c6bf49276035cf4b743d36d82c2856
Robin Reigstad <robin.reigstad@gmail.com>
2020-09-02 21:04:50 -0700

    Remove old code

--- SwarmOS/old/decl/Memory/Flags.d.ts
+++
@@ -1,7 +0,0 @@
-/** Flag Memory */
-declare interface FlagProcess_Memory extends MemBase {
-    flagID: FlagID;
-}
-
-/** Flags Extension memory */
-declare type FlagExtensionsMemory = MemBase & IDictionary<FlagID, PID>
--- SwarmOS/old/src/ColonyManagement/index.ts
+++
@@ -1,7 +0,0 @@
-import { OSPackage as CLI } from "ColonyManagement/SwarmCLI";
-
-export const ColonyManagementPackage: IPackage<{}> = {
-    install(processRegistry: IProcessRegistry, extensionRegistry: IExtensionRegistry) {
-        CLI.install(processRegistry, extensionRegistry);
-    }
-}
