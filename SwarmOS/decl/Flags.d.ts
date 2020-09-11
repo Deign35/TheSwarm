bd2afd6a861f6b3b36cb0b199dfb981a1535a0ee
Robin Reigstad <robin.reigstad@gmail.com>
2020-09-10 19:42:44 -0700

    Update flag memory

--- SwarmOS/decl/Flags.d.ts
+++ SwarmOS/decl/Flags.d.ts
@@ -1,7 +1,2 @@
-/** Flag Memory */
-declare interface FlagProcess_Memory extends MemBase {
-  flagID: FlagID;
-}
-
 /** Flags Extension memory */
-declare type FlagManagerMemory = MemBase & IDictionary<FlagID, PID>
+declare interface FlagManagerMemory extends MemBase { }
