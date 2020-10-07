90127ce023c419a4a95469b058d3c8c8792150f3
Deign <robin.reigstad@gmail.com>
2020-10-06 20:58:44 -0700

    Change from flower to bunker

--- SwarmOS/src/Rooms/RoomPlanner.ts
+++ SwarmOS/src/Rooms/RoomPlanner.ts
@@ -22,7 +22,7 @@ class RoomPlanner extends BasicProcess<RoomPlanner_Memory, MemCache> {
         // Already plopped down a spawn.
         this.memory.anchorPosX = spawns[0].pos.x - 4;
         this.memory.anchorPosY = spawns[0].pos.y;
-        if (wallDist[this.memory.anchorPosX * 50 + this.memory.anchorPosY] >= 8) {
+        if (wallDist[this.memory.anchorPosX * 50 + this.memory.anchorPosY] >= 7) {
           this.memory.canFlower = true;
         } else {
           this.memory.canFlower = false;
@@ -52,7 +52,7 @@ class RoomPlanner extends BasicProcess<RoomPlanner_Memory, MemCache> {
       let lowestValley = Infinity;
       let peakIndex = -1;
       for (let i = 0; i < peaks.length; i++) {
-        if (wallDist[peaks[i]] >= 8) {
+        if (wallDist[peaks[i]] >= 7) {
           if (combinedDistanceMatrix[peaks[i]] < lowestValley) {
             lowestValley = combinedDistanceMatrix[peaks[i]];
             peakIndex = peaks[i];
@@ -75,12 +75,12 @@ class RoomPlanner extends BasicProcess<RoomPlanner_Memory, MemCache> {
     const adjX = 25 - this.memory.anchorPosX;
     const adjY = 25 - this.memory.anchorPosY;
     let hasCreatedSites = false;
-    for (let structType in Flower) {
+    for (let structType in Bunker) {
       if (structType === STRUCTURE_ROAD && room.controller.level < 3) { continue; }
       const numAllowed = CONTROLLER_STRUCTURES[structType][room.controller.level];
-      const numTotal = Math.min(numAllowed, Flower[structType].length);
+      const numTotal = Math.min(numAllowed, Bunker[structType].length);
       for (let i = 0; i < numTotal; i++) {
-        if (room.createConstructionSite(Flower[structType][i].x - adjX, Flower[structType][i].y - adjY, structType as BuildableStructureConstant) == OK) {
+        if (room.createConstructionSite(Bunker[structType][i].x - adjX, Bunker[structType][i].y - adjY, structType as BuildableStructureConstant) == OK) {
           hasCreatedSites = true;
         }
       }
@@ -90,20 +90,18 @@ class RoomPlanner extends BasicProcess<RoomPlanner_Memory, MemCache> {
 
     const sources = room.find(FIND_SOURCES);
     const roomData = this.roomManager.GetRoomData(this.memory.homeRoom)!;
-    if (room.controller.level >= 3) {
-      let numberOfSites = Object.keys(Game.constructionSites).length;
-      for (let i = 0; i < sources.length; i++) {
-        numberOfSites += this.CreatePathToPosition(sources[i].pos, true, numberOfSites);
-      }
+    let numberOfSites = Object.keys(Game.constructionSites).length;
+    for (let i = 0; i < sources.length; i++) {
+      numberOfSites += this.CreatePathToPosition(sources[i].pos, true, numberOfSites, room.controller.level >= 3);
+    }
 
-      numberOfSites += this.CreatePathToPosition(room.controller.pos, false, numberOfSites);
-      if (room.controller.level >= 6) {
-        for (let i = 0; i < roomData.mineralIDs.length; i++) {
-          const mineral = Game.getObjectById<Mineral>(roomData.mineralIDs[i]);
-          if (mineral) {
-            numberOfSites += this.CreatePathToPosition(mineral.pos, true, numberOfSites);
-            room.createConstructionSite(mineral.pos.x, mineral.pos.y, STRUCTURE_EXTRACTOR);
-          }
+    numberOfSites += this.CreatePathToPosition(room.controller.pos, false, numberOfSites, room.controller.level >= 3);
+    if (room.controller.level >= 6) {
+      for (let i = 0; i < roomData.mineralIDs.length; i++) {
+        const mineral = Game.getObjectById<Mineral>(roomData.mineralIDs[i]);
+        if (mineral) {
+          numberOfSites += this.CreatePathToPosition(mineral.pos, true, numberOfSites, room.controller.level >= 3);
+          room.createConstructionSite(mineral.pos.x, mineral.pos.y, STRUCTURE_EXTRACTOR);
         }
       }
     }
@@ -162,7 +160,7 @@ class RoomPlanner extends BasicProcess<RoomPlanner_Memory, MemCache> {
     return ThreadState_Done;
   }
 
-  CreatePathToPosition(pos: RoomPosition, createContainer: boolean, numberOfSites: number) {
+  CreatePathToPosition(pos: RoomPosition, createContainer: boolean, numberOfSites: number, buildRoad: boolean) {
     const path = pos.findPathTo(this.memory.anchorPosX, this.memory.anchorPosY, {
       ignoreCreeps: true,
       ignoreDestructibleStructures: false,
@@ -176,19 +174,12 @@ class RoomPlanner extends BasicProcess<RoomPlanner_Memory, MemCache> {
 
     const room = Game.rooms[this.memory.homeRoom];
     let numSitesCreated = 0;
-    for (let i = 0; i < path.length; i++) {
-      if (i == 0 && createContainer) {
-        room.createConstructionSite(path[i].x, path[i].y, STRUCTURE_CONTAINER);
-        numSitesCreated++;
-      } else {
-        const foundAtPosition = room.lookAt(path[i].x, path[i].y);
-        for (let j = 0; j < foundAtPosition.length; j++) {
-          if (foundAtPosition[j].type == LOOK_CONSTRUCTION_SITES ||
-            foundAtPosition[j].type == LOOK_STRUCTURES) {
-            return numSitesCreated;
-          }
-        }
-
+    if (path.length > 0 && createContainer) {
+      room.createConstructionSite(path[0].x, path[0].y, STRUCTURE_CONTAINER);
+      numSitesCreated++;
+    }
+    if (buildRoad) {
+      for (let i = createContainer ? 1 : 0; i < path.length - 7; i++) {
         room.createConstructionSite(path[i].x, path[i].y, STRUCTURE_ROAD);
         numSitesCreated++;
       }
@@ -198,8 +189,8 @@ class RoomPlanner extends BasicProcess<RoomPlanner_Memory, MemCache> {
   }
 }
 
-// https://tinyurl.com/yxzu7w5c
-const Flower: { [id: string]: { x: number, y: number }[] } = {
+// https://tinyurl.com/yxrs5dkb
+const Bunker: { [id: string]: { x: number, y: number }[] } = {
   [STRUCTURE_TOWER]: [
     { x: 25, y: 22 }, { x: 24, y: 23 }, { x: 22, y: 25 }, { x: 28, y: 25 }, { x: 27, y: 26 }, { x: 25, y: 28 }
   ],
@@ -207,37 +198,40 @@ const Flower: { [id: string]: { x: number, y: number }[] } = {
     { x: 29, y: 25 }, { x: 25, y: 21 }, { x: 25, y: 29 }
   ],
   [STRUCTURE_EXTENSION]: [
-    { x: 19, y: 19 }, { x: 20, y: 19 }, { x: 21, y: 19 }, { x: 29, y: 19 }, { x: 30, y: 19 }, { x: 31, y: 19 },
-    { x: 19, y: 20 }, { x: 21, y: 20 }, { x: 22, y: 20 }, { x: 28, y: 20 }, { x: 29, y: 20 }, { x: 31, y: 20 },
-    { x: 19, y: 21 }, { x: 20, y: 21 }, { x: 22, y: 21 }, { x: 23, y: 21 }, { x: 27, y: 21 }, { x: 28, y: 21 }, { x: 30, y: 21 }, { x: 31, y: 21 },
-    { x: 20, y: 22 }, { x: 21, y: 22 }, { x: 23, y: 22 }, { x: 27, y: 22 }, { x: 29, y: 22 }, { x: 30, y: 22 },
-    { x: 21, y: 23 }, { x: 22, y: 23 }, { x: 28, y: 23 }, { x: 29, y: 23 },
+    { x: 23, y: 19 }, { x: 25, y: 19 }, { x: 27, y: 19 }, { x: 24, y: 20 }, { x: 26, y: 20 }, // Top
 
-    { x: 19, y: 31 }, { x: 20, y: 31 }, { x: 21, y: 31 }, { x: 29, y: 31 }, { x: 30, y: 31 }, { x: 31, y: 31 },
-    { x: 19, y: 30 }, { x: 21, y: 30 }, { x: 22, y: 30 }, { x: 28, y: 30 }, { x: 29, y: 30 }, { x: 31, y: 30 },
-    { x: 19, y: 29 }, { x: 20, y: 29 }, { x: 22, y: 29 }, { x: 23, y: 29 }, { x: 27, y: 29 }, { x: 28, y: 29 }, { x: 30, y: 29 }, { x: 31, y: 29 },
-    { x: 20, y: 28 }, { x: 21, y: 28 }, { x: 23, y: 28 }, { x: 27, y: 28 }, { x: 29, y: 28 }, { x: 30, y: 28 },
-    { x: 21, y: 27 }, { x: 22, y: 27 }, { x: 28, y: 27 }, { x: 29, y: 27 },
+    { x: 28, y: 20 }, { x: 29, y: 20 }, { x: 29, y: 21 }, { x: 30, y: 21 }, { x: 30, y: 22 }, // Outer Top Right
+    { x: 27, y: 21 }, { x: 27, y: 22 }, { x: 28, y: 22 }, { x: 28, y: 23 }, { x: 29, y: 23 }, // Inner Top Right
+
+    { x: 31, y: 23 }, { x: 30, y: 24 }, { x: 31, y: 25 }, { x: 30, y: 26 }, { x: 31, y: 27 }, // Right
+
+    { x: 30, y: 28 }, { x: 30, y: 29 }, { x: 29, y: 29 }, { x: 29, y: 30 }, { x: 28, y: 30 }, // Outer Bottom Right
+    { x: 29, y: 27 }, { x: 28, y: 27 }, { x: 28, y: 28 }, { x: 27, y: 28 }, { x: 27, y: 29 }, // Inner Bottom Right
+
+    { x: 23, y: 31 }, { x: 24, y: 30 }, { x: 25, y: 31 }, { x: 26, y: 30 }, { x: 27, y: 31 }, // Bottom
+
+    { x: 20, y: 28 }, { x: 20, y: 29 }, { x: 21, y: 29 }, { x: 21, y: 30 }, { x: 22, y: 30 }, // Outer Bottom Left
+    { x: 21, y: 27 }, { x: 22, y: 27 }, { x: 22, y: 28 }, { x: 23, y: 28 }, { x: 23, y: 29 }, // Inner Bottom Left
+
+    { x: 19, y: 23 }, { x: 19, y: 25 }, { x: 19, y: 27 }, { x: 20, y: 24 }, { x: 20, y: 26 }, // Left
+
+    { x: 22, y: 20 }, { x: 21, y: 20 }, { x: 21, y: 21 }, { x: 20, y: 21 }, { x: 20, y: 22 }, // Outer Top Left
+    { x: 23, y: 21 }, { x: 23, y: 22 }, { x: 22, y: 22 }, { x: 22, y: 23 }, { x: 21, y: 23 }, // Inner Top Left
   ],
-  [STRUCTURE_STORAGE]: [{ x: 23, y: 24 }],
+  [STRUCTURE_STORAGE]: [{ x: 26, y: 23 }],
   [STRUCTURE_LINK]: [{ x: 21, y: 25 }],
   [STRUCTURE_POWER_SPAWN]: [{ x: 25, y: 23 }],
-  [STRUCTURE_TERMINAL]: [{ x: 26, y: 27 }],
+  [STRUCTURE_TERMINAL]: [{ x: 27, y: 24 }],
   [STRUCTURE_FACTORY]: [{ x: 27, y: 25 }],
-  [STRUCTURE_OBSERVER]: [{ x: 27, y: 24 }],
+  [STRUCTURE_OBSERVER]: [{ x: 26, y: 27 }],
   [STRUCTURE_LAB]: [
     { x: 24, y: 24 }, { x: 25, y: 24 },
     { x: 23, y: 25 }, { x: 24, y: 25 }, { x: 26, y: 25 },
     { x: 23, y: 26 }, { x: 25, y: 26 }, { x: 26, y: 26 },
     { x: 24, y: 27 }, { x: 25, y: 27 },
   ],
-  [STRUCTURE_NUKER]: [{ x: 26, y: 23 }],
+  [STRUCTURE_NUKER]: [{ x: 23, y: 24 }],
   [STRUCTURE_ROAD]: [
-    { x: 20, y: 20 }, { x: 21, y: 21 }, { x: 22, y: 22 }, { x: 23, y: 23 },
-    { x: 30, y: 20 }, { x: 29, y: 21 }, { x: 28, y: 22 }, { x: 27, y: 23 },
-    { x: 20, y: 30 }, { x: 21, y: 29 }, { x: 22, y: 28 }, { x: 23, y: 27 },
-    { x: 30, y: 30 }, { x: 29, y: 29 }, { x: 28, y: 28 }, { x: 27, y: 27 },
-
     { x: 25, y: 20 }, { x: 24, y: 21 }, { x: 26, y: 21 }, { x: 24, y: 22 }, { x: 26, y: 22 },
     { x: 20, y: 25 }, { x: 21, y: 24 }, { x: 21, y: 26 }, { x: 22, y: 24 }, { x: 22, y: 26 },
     { x: 30, y: 25 }, { x: 29, y: 24 }, { x: 29, y: 26 }, { x: 28, y: 24 }, { x: 28, y: 26 },
@@ -245,20 +239,18 @@ const Flower: { [id: string]: { x: number, y: number }[] } = {
 
     { x: 24, y: 26 }, { x: 25, y: 25 }, { x: 26, y: 24 },
 
-    { x: 19, y: 18 }, { x: 20, y: 18 }, { x: 21, y: 18 }, { x: 29, y: 18 }, { x: 30, y: 18 }, { x: 31, y: 18 },
-    { x: 18, y: 19 }, { x: 22, y: 19 }, { x: 28, y: 19 }, { x: 32, y: 19 },
-    { x: 18, y: 20 }, { x: 23, y: 20 }, { x: 27, y: 20 }, { x: 32, y: 20 },
-    { x: 18, y: 21 }, { x: 32, y: 21 },
-    { x: 19, y: 22 }, { x: 31, y: 22 },
-    { x: 20, y: 23 }, { x: 30, y: 23 },
+    { x: 21, y: 19 }, { x: 22, y: 19 }, { x: 24, y: 19 }, { x: 26, y: 19 }, { x: 28, y: 19 }, { x: 29, y: 19 },
+    { x: 20, y: 20 }, { x: 23, y: 20 }, { x: 27, y: 20 }, { x: 30, y: 20 },
+    { x: 19, y: 21 }, { x: 22, y: 21 }, { x: 28, y: 21 }, { x: 31, y: 21 },
+    { x: 19, y: 22 }, { x: 21, y: 22 }, { x: 29, y: 22 }, { x: 31, y: 22 },
+    { x: 20, y: 23 }, { x: 23, y: 23 }, { x: 27, y: 23 }, { x: 30, y: 23 },
+    { x: 19, y: 24 }, { x: 31, y: 24 },
 
-    { x: 20, y: 27 }, { x: 30, y: 27 },
-    { x: 19, y: 28 }, { x: 31, y: 28 },
-    { x: 18, y: 29 }, { x: 32, y: 29 },
-    { x: 18, y: 30 }, { x: 23, y: 30 }, { x: 27, y: 30 }, { x: 32, y: 30 },
-    { x: 18, y: 31 }, { x: 22, y: 31 }, { x: 28, y: 31 }, { x: 32, y: 31 },
-    { x: 19, y: 32 }, { x: 20, y: 32 }, { x: 21, y: 32 }, { x: 29, y: 32 }, { x: 30, y: 32 }, { x: 31, y: 32 },
+    { x: 19, y: 26 }, { x: 31, y: 26 },
+    { x: 20, y: 27 }, { x: 23, y: 27 }, { x: 27, y: 27 }, { x: 30, y: 27 },
+    { x: 19, y: 28 }, { x: 21, y: 28 }, { x: 29, y: 28 }, { x: 31, y: 28 },
+    { x: 19, y: 29 }, { x: 22, y: 29 }, { x: 28, y: 29 }, { x: 31, y: 29 },
+    { x: 20, y: 30 }, { x: 23, y: 30 }, { x: 27, y: 30 }, { x: 30, y: 30 },
+    { x: 21, y: 31 }, { x: 22, y: 31 }, { x: 24, y: 31 }, { x: 26, y: 31 }, { x: 28, y: 31 }, { x: 29, y: 31 },
   ]
 }
-
-// https://tinyurl.com/y3lcqtyc
